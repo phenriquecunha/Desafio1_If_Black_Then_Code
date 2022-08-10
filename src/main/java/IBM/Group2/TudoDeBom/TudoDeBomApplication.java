6eedf04934faa1b5bc7c2de31417fe155fc8fd94
phenriquecunha <phcs-12@hotmail.com>
2022-08-10 20:05:01 -0300

    First commit, initial project structure

---
+++ src/main/java/IBM/Group2/TudoDeBom/TudoDeBomApplication.java
@@ -0,0 +1,13 @@
+package IBM.Group2.TudoDeBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudoDeBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudoDeBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IBM/Group2/TudoDeBom/TudoDeBomApplicationTests.java
@@ -0,0 +1,13 @@
+package IBM.Group2.TudoDeBom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudoDeBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
