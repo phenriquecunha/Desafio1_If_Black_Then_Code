6cc682d6a16dd66b3083d16e7f26acabcd6fb2d5
phenriquecunha <phcs-12@hotmail.com>
2022-08-17 10:40:42 -0300

    Ignore MainController

--- src/main/java/IBM/Group2/TudoDeBom/Controllers/MainController.java
+++ src/main/java/IBM/Group2/TudoDeBom/Controllers/MainController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/")
+@ApiIgnore
 public class MainController {
 
   @GetMapping
